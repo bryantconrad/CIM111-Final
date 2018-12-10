a2a8bdec58048643f20bad244557ed3bb29896c4
bryantconrad <bryant.conradl@gmail.com>
2018-12-09 21:24:18 -0500

    fixed the library

--- js/expscript.js
+++ js/expscript.js
@@ -1,9 +1,26 @@
 $(document).ready(function() {
 
-    $('.tabs').tabslet({
-        mouseevent: 'click',
-        attribute: 'href',
+    $(".tabs").tabslet({
+        mouseevent: "click",
+        attribute: "href",
         animation: true
+    });
+
+    $("#expgridall").hide();
+
+    var hideStatus = true;
+
+    $("#showbtn").click(function(){
+        if(hideStatus == true){
+            $("#expgridall").slideDown(300);
+            hideStatus = false;
+            $("#showbtn").html("Hide all");
+        }
+        else{
+            $("#expgridall").slideUp(300);
+            hideStatus = true;
+            $("#showbtn").html("Show all");
+        }
     });
 
     console.log("working");
